64af5d616f33a9810457865256611b79e5a66dbb
Leonardo Mansur <punkviolence272@gmail.com>
2023-11-13 23:07:33 -0300

    FizzBuzz!

---
+++ fizzbuzz.py
@@ -0,0 +1,16 @@
+'''
+Write a short program that prints each number from 1 to 100 on a new line. 
+For each multiple of 3, print "Fizz" instead of the number. 
+For each multiple of 5, print "Buzz" instead of the number. 
+For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
+'''
+
+for n in range(1, 101):
+    if n % 3 == 0 and n % 5 == 0:
+        print('FizzBuzz')
+    elif n % 3 == 0:
+        print('Fizz')
+    elif n % 5 == 0:
+        print('Buzz')
+    else:
+        print(n)
